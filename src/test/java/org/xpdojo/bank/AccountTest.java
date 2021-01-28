1ed55aa78487888999ce5026d58af3f41f0e8e21
Raj Sudra <rsudra@hotmail.com>
2021-01-28 15:11:44 +0000

    new account balance is empty

--- src/test/java/org/xpdojo/bank/AccountTest.java
+++ src/test/java/org/xpdojo/bank/AccountTest.java
@@ -8,6 +8,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class AccountTest {
 
     @Test
+    public void newBankAccountIsEmpty() {
+        Account account = new Account();
+        assertThat(account.balance()).isEqualTo(0);
+    }
+
+    @Test
 	@Disabled
     public void depositAnAmountToIncreaseTheBalance() {
         assertThat("your first test").isBlank();
